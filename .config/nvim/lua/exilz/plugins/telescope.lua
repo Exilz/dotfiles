640c14cb7875a08eb6005c7c8a345a361cd94a13
Maxime Bertonnier <m.bertonnier@gmail.com>
2024-05-29 18:17:13 +0200

    Find hidden files with Telescope

--- .config/nvim/lua/exilz/plugins/telescope.lua
+++ .config/nvim/lua/exilz/plugins/telescope.lua
@@ -21,6 +21,12 @@ return {
 					},
 				},
 			},
+			pickers = {
+				find_files = {
+					-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+				},
+			},
 		})
 
 		telescope.load_extension("fzf")
