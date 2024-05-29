09f59ae364d9dece53ce33968f8fc748bb9e220b
Maxime Bertonnier <m.bertonnier@gmail.com>
2024-05-29 18:17:27 +0200

    Live grep inside hidden files with Telescope

--- .config/nvim/lua/exilz/plugins/telescope.lua
+++ .config/nvim/lua/exilz/plugins/telescope.lua
@@ -26,6 +26,11 @@ return {
 					-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
 					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
 				},
+				live_grep = {
+					additional_args = function(opts)
+						return { "--hidden" }
+					end,
+				},
 			},
 		})
 
