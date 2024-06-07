7b9761c83fbfb1122f774c100765a2868abe56f8
Maxime Bertonnier <m.bertonnier@gmail.com>
2024-06-07 14:04:06 +0200

    Fix go to definition in Elixir

--- .config/nvim/lua/exilz/plugins/elixir-tools.lua
+++ .config/nvim/lua/exilz/plugins/elixir-tools.lua
@@ -4,21 +4,25 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		local elixir = require("elixir")
-		local elixirls = require("elixir.elixirls")
 
 		elixir.setup({
-			nextls = { enable = true },
-			credo = {},
+			nextls = { enable = false },
+			credo = { enable = false },
 			elixirls = {
 				enable = true,
-				settings = elixirls.settings({
-					dialyzerEnabled = false,
-					enableTestLenses = false,
-				}),
 				on_attach = function(client, bufnr)
+					local opts = { noremap = true, silent = true }
+					opts.buffer = bufnr
+
 					vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
 					vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
 					vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+
+					-- Rebind go to definition, reference & implementation beacuse Telescope
+					-- isn't able to find them for some reason (elixir only)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 				end,
 			},
 		})
