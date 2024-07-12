038c0f9adab2d52a8b469493c71b3d0a7094defc
Maxime Bertonnier <m.bertonnier@gmail.com>
2024-07-12 17:14:23 +0200

    Add nvim cmd to toggle autoformat

--- .config/nvim/lua/exilz/plugins/conform.lua
+++ .config/nvim/lua/exilz/plugins/conform.lua
@@ -23,11 +23,13 @@ return {
 				python = { "isort", "black" },
 				go = { "goimports", "gofmt" },
 			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
+			format_on_save = function(bufnr)
+				-- Disable with a global or buffer-local variable
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				return { timeout_ms = 500, lsp_format = "fallback" }
+			end,
 		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>lf", function()
@@ -38,5 +40,25 @@ return {
 			})
 			require("lint").try_lint()
 		end, { desc = "Format file or range (in visual mode)" })
+
+		-- Add commands to enable/disable autoformatting
+		-- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				-- FormatDisable! will disable formatting just for this buffer
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
 	end,
 }
